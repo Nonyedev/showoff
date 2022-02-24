01977160763d1ea4f2455e4fc28c5ccefd58a642
special-spoon <enweremchinonyejudith2003@gmail.com>
2022-02-24 20:33:34 +0100

    style.css

---
+++ welcome.php
@@ -0,0 +1,223 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Showoff website</title>
+<link rel="stylesheet" href="style.css">   
+</head>
+    <body>
+    <section class="header">
+    <nav>
+    <a href="index.html"><img src="images/advert.jpg"></a>
+    <div class="nav-links" id="navLinks">
+        <i class="fa" onclick="hideMenu()">&#215;</i>
+    <ul>
+    <li><a href="">HOME</a></li>
+    <li><a href="">ABOUT</a></li>    
+    <li><a href="">SERVICES</a></li>    
+    <li><a href="">BLOG</a></li>    
+    <li><a href="">CONTACT</a></li>    
+    </ul>    
+    </div>
+        <i class="fa" onclick="showMenu()">&#9776;</i>
+    </nav>
+        
+        <div class="text-box">
+        <h1>SHOWOFF SALON</h1>
+        <p>Get Professional Services That's Sure To Turn Heads... <br>a salon where beauty and wellness align!</p>
+        <a href="" class="hero-btn">Book Services Now</a>
+        </div>
+    </section>
+        
+        <!-------- course -------->
+        
+        <section class="course">
+        <h1>Why ShowOFF</h1>
+        <p>Take your time to read through in order to find out why showoff is best amongst others and you will be glad you did.</p>
+            
+            <div class="row">
+               <div class="course-col">
+                 <h3>The Name Showoff</h3>
+                 <p>There are many salons in Owerri, Imo state as the demand for beauty continues to grow. So why would i yet create another salon? And, what makes my concept different?... Let's dive in!</p>
+        <p>The name SHOWOFF is mearnt to illustrate/showcase the professionalism, efficiency, speed and quality in our variety of services available in this modern day. Showoff is a long time DREAM that have come to actualization and whose biggest aspiration is to render services that will make our clients have that self confidence and to stand out amongst others. Giving our clients the oppourtunity to look their best without having to sacrifice time and quality. We are unique as we make beauty fast, casual and effortless.</p>
+               
+                </div><div class="course-col">
+                 <h3>Health and Safety</h3>
+                 <p>Taking care of yourself is about health and beauty. As a business owner and leader, i know that keeping my customers and my staffs safe is the top priority. Our team wants you to feel and look your best and the only way we can truly accomplish that is by maintaining a clean and safe environment.</p>
+            <p>health and Safety are part of our everyday environment as we continue to protect our staffs and clients. Showoff will continue to be a safe environment due to the design and investment of our salon.</p>
+               
+                </div><div class="course-col">
+                 <h3>Innovative Technology</h3>
+                 <p>Imagine stepping into a beautiful space where you indulge in all your beauty bliss while saving time...  isn't it a dream come true?!</p>
+            <p>Our gorgeous salon is home to a variety of technology that makes it possible for us to provide beauty services efficiently and safely.</p>
+               </div>
+            </div>
+        </section>
+        
+        
+        <!----- campus ----->
+        
+        <section class="campus">
+           <h1>HIGHLIGHT</h1>
+            <p>Showoff is dedicated to offering all your service needs in one salon-spa setting. We not only focus on our staple services of lashes, hair, facials, nails, pedicure and manicure, we also pamper our clients in a comfortable and luxurious setting, all in an efficient time frame.</p>
+            
+        <div class="row">
+            <div class="campus-col">
+            <img src="images/barberside.jpg">
+                <div class="layer">
+                <h3>Barbing Section</h3>
+                </div>
+            </div>
+            
+            <div class="campus-col">
+            <img src="images/gameguys.jpg">
+                <div class="layer">
+                <h3>Entertainment</h3>
+                </div>
+            </div>
+            
+            <div class="campus-col">
+            <img src="images/hairsection.jpg">
+                <div class="layer">
+                <h3>Female Section</h3>
+                </div>
+            </div>
+            
+        </div>
+        </section>
+        
+        
+        
+        
+        
+        
+        <!----- facilities ----->
+        
+        <section class="faci">
+        <h1>Our Services</h1>
+        <p>The Beauty industry future is Beautiful, The future is SHOWOFF.</p>
+        
+        <div class="row">
+            <div class="faci-col">
+            <img src="images/haircut.jpg">
+                <h3>Our Haircut</h3>
+                <p>Our haircuts are one of a kind. With the help of our professional barbers, we render a classy haircut to our clients. Whether you want a complete barbing; which comes with barbing, dying, drying, and washing or just barbing even carving/shaving, we gat you! cos you will leave looking amazing and your haircut will still look good days later.</p>
+            </div>
+            
+            <div class="faci-col">
+            <img src="images/pedimachine.jpg">
+                <h3>Pedicure</h3>
+                <p>You've never had a pedicure? We'll fill you in. Every refined lady or gentleman knows that a pedicured feet makes you feel more comfortable and confident.</p>
+                <p>You also understand that when you get a pedicure, you want a feet and toenails that feel good, look good and last longer than just a few days, which is why SHOWOFF is thrilled to officially be offering professional pedicure (Solvent and Gelly) and you get to sit in our massaging machine and enjoy a full body massage that gets you relaxed while sipping your champagne.</p>
+            </div>
+            
+            <div class="faci-col">
+            <img src="images/nailbar.jpg">
+                <h3>Facials</h3>
+                <p>Put Your Best Face Forward...You can't deny the fact that your face says it all. Oh! that feeling when your face is rid of pimples and totally taken care of by our trained professionals... hmmmmm! so sleek and attractive, you can imagine.</p>
+                <p>So why waste time?... Rush to Showoff now to get your lovely face treated with the right and quality products that will leave your face refreshed all week long.</p>
+            </div>
+        </div>
+        </section>
+        
+        
+        
+        <section class="faci2">
+        <div class="row">
+            <div class="faci-col">
+            <img src="images/nailbar.jpg">
+                <h3>Nails/manicure</h3>
+                <p>Beautiful nails make all the difference, and you deserve the best. Between typing on your laptop, texting your friends and everything else in between, you need to have a manicure meant for a modern woman. Our professionalism in nails fixing can help your natural nail growth and give you a beter feel. </p>
+                <p>Whether you want a trendy coffin shape, a clean shorter nail or any lenght you choose, we deliver perfectly.</p>
+            </div>
+            
+            <div class="faci-col">
+            <img src="images/makeupsection.jpg">
+                <h3>Makeup</h3>
+                <p>Wake Up and Make Up!... Get professionally contoured, makeup look that will deliver our signature to the world. You feel more confident, look chic and bring a bit of class to every outfit, day or night.</p>
+                <p>If you want to be that Showoff babe, then let's get started!</p>
+            </div>
+            
+            <div class="faci-col">
+            <img src="images/hairsection.jpg">
+                <h3>Hairstyles</h3>
+                <p>With our various Hairstyles (weaving, braiding and crocheting) and our wigging (normal, closure and frontal fixing), your are sure to stand out amongst other ladies.</p>
+            </div>
+        </div>
+        
+        </section>
+       
+        
+        
+        
+        
+        
+        <!----- testimonials ----->
+        
+        
+        
+        
+        <section class="test">
+        <h1>What do you think about Showoff?</h1>
+        <p>Check out this testimonials of some of our clients </p>
+            
+         <div class="row">
+             
+             <div class="test-col">
+             <img src="images/reception.jpg">
+                 <div>
+                 <p>You are missing oo if you've not been to showoff. the environment is so <br>lovely and safe.</p>
+                     <h3>Slimzy</h3>
+                 </div>
+             </div>
+             
+             <div class="test-col">
+             <img src="images/hairstyle.jpg">
+                 <div>
+                 <p>Come and see for yourself. <br>   &#128516;&#128516;&#128516;&#128516;&#128516;</p>
+                     <h3>Mira</h3>
+                 </div>
+             </div>
+                    
+         </div>
+            </section>
+        
+        
+        <!----- call to action ----->
+        
+        
+        <section class="cta">
+        <h1>Call for our services<br> anywhere from Owerri</h1>
+            <a href="">CONTACT US</a>
+        </section>
+        
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <!--------javascript for toggle menu-------->
+        <script>
+           var navLinks = document.getElementById("navLinks");
+           function showMenu(){
+                navLinks.style.right = "0";
+           }
+           function hideMenu(){
+                navLinks.style.right = "-200px";
+           }
+            
+        </script>
+    </body>
+</html>
